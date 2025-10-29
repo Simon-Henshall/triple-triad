c93f92741553c37f33e90af91960eb9c6d40e01b
Simon Henshall <simon@henshall.nz>
2025-10-29 23:29:28 +1300

    Removed extraneous file

--- front_end/js/render/hand.js
+++
@@ -1,8 +0,0 @@
-import { utils } from '../game/utils.js';
-import { offsets } from './offsets.js';
-
-export function createPlayerHandContainers(cards) {
-  return cards.map((card, index) =>
-    utils.createCardContainer(card, 'blue', offsets.handOffsetX, offsets.handOffsetY + index * offsets.handCardOffset)
-  );
-}
