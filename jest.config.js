7d4718af392e7b2f17bd21c16044ca1cb041a9f2
Simon Henshall <simon@henshall.nz>
2026-06-07 22:20:50 +1200

    Better test config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ export default {
   testEnvironment: "node",
   transform: {},
   moduleDirectories: ["node_modules"], // standard
+  verbose: true,
+  setupFilesAfterEnv: ["./front_end/js/__tests__/setup.js"],
+  testPathIgnorePatterns: ["/node_modules/", "setup.js"],
 };
