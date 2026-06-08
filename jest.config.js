99ed8fcae24c3d833ea9b4614f5553a62faf9cdf
Simon Henshall <simon@henshall.nz>
2026-06-08 19:09:17 +1200

    Jest code coverage

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,14 @@ export default {
   verbose: true,
   setupFilesAfterEnv: ["./front_end/js/__tests__/setup.js"],
   testPathIgnorePatterns: ["/node_modules/", "setup.js"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "./front_end/js/**/*.js",
+    "!./front_end/js/**/setup.js",
+  ],
+  coverageThreshold: {
+    global: {
+      lines: 90,
+    },
+  },
 };
